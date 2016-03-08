c9f2698a6e64ed618ded67d32134eaed5241ef09
Ashish Gupta <therealashishgupta@gmail.com>
2016-03-08 16:26:43 +0530

    fixed errors

--- global_config/config.php
+++ global_config/config.php
@@ -7,7 +7,7 @@
  */
 
 define('HOST', 'localhost/');
-define('BASEURL_PREFIX', 'Indiacom2015/');
+define('BASEURL_PREFIX', 'Indiacom16/');
 define('DBGROUP', 'default');
 define('SMS_SEND_LINK', "http://203.212.70.200/smpp/sendsms");
 define('SMS_SERVICE_USER_ID', 'bhartiv');
@@ -15,4 +15,5 @@ define('SMS_SERVICE_PWD', 'del12345');
 define('SMS_FROM', 'BVICAM');
 define('RECAPTCHA_LINK', 'https://www.google.com/recaptcha/api/siteverify');
 define('RECAPTCHA_SECRET', '6LcQQwYTAAAAAKGtQ968dkP7HCKcQ-PfG02jhRYp');
+//define('EVENT_ID','1');
 ?>
