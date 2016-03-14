499bef0cd319bf7c88e7cbba8e480b25315745c6
Ashish Gupta <therealashishgupta@gmail.com>
2016-03-14 15:42:32 +0530

    fixed assign member deliverables
    added the earlier removed echo statements

--- bvicam_admin/application/views/pages/DeliverablesManager/assignMemberDeliverables.php
+++ bvicam_admin/application/views/pages/DeliverablesManager/assignMemberDeliverables.php
@@ -31,9 +31,9 @@
                             <td class ="member_id" data-member_id=
                                 "<?php
                                 if(isset($payment -> payment_member_id))
-                                     $payment -> payment_member_id;
+                                    echo $payment -> payment_member_id;
                                 else if(isset($payment -> payment_submission_id))
-                                     $payment -> submission_member_id;
+                                     echo $payment -> submission_member_id;
                                 ?>"
                             >
                                 <?php
@@ -46,7 +46,7 @@
                             </td>
                             <td  class="submission_id" data-submission_id =
                                 "<?php
-                                 $payment->payment_submission_id;
+                                 echo $payment->payment_submission_id;
                                 ?>"
                             >
                                 <?php
